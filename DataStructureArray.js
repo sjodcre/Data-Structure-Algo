be8c222aaa1419649573f236740710d6dc1a1b41
unknown <samuelomamalin@gmail.com>
2019-07-11 19:52:49 +0800

    data structures

---
+++ DataStructureArray.js
@@ -0,0 +1,125 @@
+//Arrays
+// (a) Array Structure
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  deleteAtIndex(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    console.log(this.data[this.length - 1]);
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const myArray = new MyArray();
+myArray.push('hi');
+myArray.push('you');
+myArray.push('!');
+myArray.pop();
+myArray.deleteAtIndex(0);
+myArray.push('are');
+myArray.push('nice');
+myArray.shiftItems(0);
+console.log(myArray);
+
+//(b) Array Implementation
+const strings= ['a', 'b', 'c', 'd'];
+const numbers = [1,2,3,4,5];
+// Variable array is somewhere in memory and the computer knows it.
+// When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
+
+
+//push
+strings.push('e');
+
+//pop
+strings.pop();
+strings.pop();
+
+//unshift
+strings.unshift('x')
+
+//splice
+strings.splice(2, 0, 'alien');
+
+console.log(strings)
+
+//(c) Reverse a string
+function reverse(str){
+  if(!str || typeof str != 'string' || str.length < 2 ) return str;
+  
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for(let i = totalItems; i >= 0; i--){
+    backwards.push(str[i]);
+  }
+  return backwards.join('');
+}
+
+function reverse2(str){
+  //check for valid input
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+
+reverse('Timbits Hi')
+reverse('Timbits Hi')
+reverse3('Timbits Hi')
+
+// (d) Merge Sorted Arrays
+function mergeSortedArrays(array1, array2){
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  
+  //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
+  if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item){
+   if(array2Item === undefined || array1Item < array2Item){
+     mergedArray.push(array1Item);
+     array1Item = array1[i];
+     i++;
+   }   
+   else {
+     mergedArray.push(array2Item);
+     array2Item = array2[j];
+     j++;
+   }
+  }
+  return mergedArray;
+}
+
+mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+
---
+++ DataStructureStackQueue.js
@@ -0,0 +1,192 @@
+// stacks and queues
+// (a)stack implemented with linked list
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value){
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop(){
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
+
+
+//Discord
+//Udemy
+//google
+
+// (b) stack implemented with array
+class Stack {
+  constructor(){
+    this.array = [];
+  }
+  peek() {
+    return this.array[this.array.length-1];
+  }
+  push(value){
+    this.array.push(value);
+    return this;
+  }
+  pop(){
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
+
+
+//Discord
+//Udemy
+//google
+
+// (c) queue implementation
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value){
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue(){
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
+
+// (d) queues implemented using stacks
+class CrazyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
+
